38a1a9ef0f66ad0471453e617f008d2425e5cdb2
Kacper Dziedzic <kapi584@gmail.com>
2024-12-03 14:55:48 +0000

    modyfikacja zakresu dzialania do 1-10000

--- program.py
+++ program.py
@@ -1,10 +1,10 @@
 import random
 
 def zgadnij_liczbe():
-    liczba = random.randint(1, 100)
+    liczba = random.randint(1, 10000)
     proba = 0
 
-    print("Zgadnij liczbę od 1 do 100!")
+    print("Zgadnij liczbę od 1 do 10000!")
 
     while True:
         odpowiedz = int(input("Podaj swoją liczbę: "))
